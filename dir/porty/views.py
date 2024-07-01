1af041fe3c6a20e4634e942fe96db3ce6465c790
Dom Catania <24DCatania@student.nchstx.org>
2024-07-01 18:07:32 -0500

    basic layout

--- dir/porty/views.py
+++ dir/porty/views.py
@@ -1,3 +1,4 @@
 from django.shortcuts import render
 
-# Create your views here.
+def home(request) -> render:
+    return render(request, 'home.html')
---
+++ porty.bat
@@ -0,0 +1,1 @@
+python3 C:\Users\dmaca\Desktop\MyCode\Porty\dir\manage.py runserver
