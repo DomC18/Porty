4e55163c5749b5e657f64983e55409d74200004b
DomC18 <24DCatania@student.nchstx.org>
2024-07-09 17:31:32 -0500

    more revisions to animations

--- home.js
+++ home.js
@@ -1,7 +1,7 @@
 const observer = new IntersectionObserver((entries) => {
     entries.forEach((entry) => {
         console.log(entry)
-        if (entry.isIntersecting && (entry.boundingClientRect.top > 300 || entry.boundingClientRect.bottom < 1200)) {
+        if (entry.isIntersecting) {
             entry.target.classList.add('show');
         } else {
             entry.target.classList.remove('show');
